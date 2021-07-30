c33c56afc9c0ed26d100423e2a87be73fce0acd4
Michelle <mchllgndrn@gmail.com>
2021-07-30 07:31:52 -0500

    Removes dup components, renders todos

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,6 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const usersRouter = express.Router();
 const { getAllUsers, getUserById } = require("../../db/users");
+// Import, then initiate Magic instance for server-side methods
+const { Magic } = require("@magic-sdk/admin");
+const magic = new Magic(process.env.MAGIC_SECRET_KEY);
+
+// Route to validate the user's DID token
+usersRouter.post("/login", async (req, res) => {
+  try {
+    const didToken = req.headers.authorization.substr(7);
+    await magic.token.validate(didToken);
+    res.status(200).json({ authenticated: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 usersRouter.get("/", async (req, res, next) => {
   try {
@@ -31,4 +45,106 @@ usersRouter.get("/:id", async (req, res, next) => {
     });
   }
 });
+
+
+// const Datastore = require("nedb-promise");
+// let users = new Datastore({ filename: "users.db", autoload: true });
+
+/* 2️⃣ Implement Auth Strategy */
+const passport = require("passport");
+const MagicStrategy = require("passport-magic").Strategy;
+
+const strategy = new MagicStrategy(async function(user, done) {
+  const userMetadata = await magic.users.getMetadataByIssuer(user.issuer);
+  const existingUser = await users.findOne({ issuer: user.issuer });
+  if (!existingUser) {
+    /* Create new user if doesn't exist */
+    return signup(user, userMetadata, done);
+  } else {
+    /* Login user if otherwise */
+    return login(user, done);
+  }
+});
+
+passport.use(strategy);
+
+/* 3️⃣ Implement Auth Behaviors */
+
+/* Implement User Signup */
+const signup = async (user, userMetadata, done) => {
+  let newUser = {
+    issuer: user.issuer,
+    email: userMetadata.email,
+    lastLoginAt: user.claim.iat
+  };
+  await users.insert(newUser);
+  return done(null, newUser);
+};
+
+/* Implement User Login */
+const login = async (user, done) => {
+  /* Replay attack protection (https://go.magic.link/replay-attack) */
+  if (user.claim.iat <= user.lastLoginAt) {
+    return done(null, false, {
+      message: `Replay attack detected for user ${user.issuer}}.`
+    });
+  }
+  await users.update(
+    { issuer: user.issuer },
+    { $set: { lastLoginAt: user.claim.iat } }
+  );
+  return done(null, user);
+};
+
+/* Attach middleware to login endpoint */
+usersRouter.post("/login", passport.authenticate("magic"), (req, res) => {
+  if (req.user) {
+      res.status(200).end('User is logged in.');
+  } else {
+     return res.status(401).end('Could not log user in.');
+  }
+});
+
+/* 4️⃣ Implement Session Behavior */
+
+/* Defines what data are stored in the user session */
+passport.serializeUser((user, done) => {
+  done(null, user.issuer);
+});
+
+/* Populates user data in the req.user object */
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await users.findOne({ issuer: id });
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+/* 5️⃣ Implement User Endpoints */
+
+/* Implement Get Data Endpoint */
+usersRouter.get("/", async (req, res) => {
+  if (req.isAuthenticated()) {
+    return res
+      .status(200)
+      .json(req.user)
+      .end();
+  } else {
+    return res.status(401).end(`User is not logged in.`);
+  } 
+});
+
+/* Implement Logout Endpoint */
+usersRouter.post("/logout", async (req, res) => {
+  if (req.isAuthenticated()) {
+    await magic.users.logoutByIssuer(req.user.issuer);
+    req.logout();
+    return res.status(200).end();
+  } else {
+    return res.status(401).end(`User is not logged in.`);
+  }
+});
+
 module.exports = usersRouter;
